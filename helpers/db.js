a5f621365ac054da81af3615e1fd9a05a52fff68
Karina Finegan <kairosity@users.noreply.github.com>
2021-04-14 16:45:38 +0100

    Delete db.js

--- helpers/db.js
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from 'mongodb'
-
-export async function connectToDatabase(){
-    const client = await MongoClient.connect('mongodb+srv://henri:iTDD2952vnkhZMbS@cluster0.todca.mongodb.net/next-blog?retryWrites=true&w=majority'
-    );
-
-    return client;
-}
